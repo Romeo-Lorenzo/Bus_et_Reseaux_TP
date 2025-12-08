f95a44f7156ba128feb1237e3f2a50551f75e2a1
Romeo-Lorenzo <161328738+Romeo-Lorenzo@users.noreply.github.com>
2025-12-08 16:54:32 +0100

    Update hello.py

--- SOFTWARE/Python/hello.py
+++ SOFTWARE/Python/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify, abort, render_template
 
 app = Flask(__name__)
 @app.route('/')
@@ -13,4 +13,12 @@ def api_welcome():
 
 @app.route('/api/welcome/<int:index>')
 def api_welcome_index(index):
-    return jsonify({"index": index, "val": welcome[index]}), {"Content-Type": "application/json"}
+    if index != len(welcome):
+        abort(404)
+    else:
+        return jsonify({"index": index, "val": welcome[index]}), {"Content-Type": "application/json"}
+    abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
